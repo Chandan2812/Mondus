6ee269ce600fb2365110e1fc4507a965531553c1
shubham9049 <shubham9049>
2025-05-06 17:40:05 +0530

    Add text opacity in hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -48,7 +48,7 @@ export default function Hero() {
           We bring <span className="font-semibold">Due Diligence</span> at Your
           service
         </p>
-        <button className="border border-[#C29579] text-[#C29579] px-6 py-3 hover:bg-[#C29579]/20 transition dark:text-[#C29579] dark:border-[#C29579] dark:hover:bg-[#C29579]/20">
+        <button className="border border-[#C29579] text-[#C29579] px-6 py-3 hover:bg-[#C29579]/20 transition dark:text-[#f7b48a] dark:border-[#C29579] dark:hover:bg-[#C29579]/20">
           Leave a request
         </button>
       </div>
