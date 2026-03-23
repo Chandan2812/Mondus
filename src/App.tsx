3fc78a4a120ed67a4324188156c6b1a4e93e6a36
Anshu-WebD <anshuweb@bigwigmediadigital.com>
2026-03-23 18:45:33 +0530

    updatd sitemap and robot.txt file

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,7 @@ import { Toaster } from "react-hot-toast";
 import PropertyManagement from "./pages/admin/PropertyManagement";
 import DeveloperManagement from "./pages/admin/DeveloperManagement";
 
-// ✅ Custom wrapper to access location inside App
+// Custom wrapper to access location inside App
 function AppWrapper() {
   const location = useLocation();
 
